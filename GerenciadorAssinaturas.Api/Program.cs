c8d204be726f63ae0a327ab60ca98c0a45b41eb7
Cervelati <luizhenriquesilvacervelati@gmail.com>
2026-08-09 16:54:06 -0300

    chore: add Scalar AspNetCore

--- GerenciadorAssinaturas.Api/Program.cs
+++ GerenciadorAssinaturas.Api/Program.cs
@@ -1,5 +1,6 @@
 using GerenciadorAssinaturas.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +9,19 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<GerenciadorDbContext>(
     options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<AssinaturaService>();
+builder.Services.AddScoped<CartaoService>();
+builder.Services.AddScoped<ClienteService>();
+builder.Services.AddScoped<PlanoService>();
+builder.Services.AddScoped<UsuarioService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
